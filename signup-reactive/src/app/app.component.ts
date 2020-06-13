c6589d8d72919d290b4db5e638d85921384f684e
GanjikuntaNikhitha <nikhitha.ganjigunta62@gmail.com>
2020-06-13 23:38:12 +0530

    Reactive forms(Brain Part)

--- signup-reactive/src/app/app.component.ts
+++ signup-reactive/src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit } from '@angular/core';
 
+import {FormBuilder,FormGroup,Validators} from '@angular/forms'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'signup-reactive';
+  registerForm : FormGroup;
+  submitted =false;
+  constructor( private formbuilder : FormBuilder){}
+  ngOnInit() {
+    this.registerForm = this.formbuilder.group({
+      firstName : ['',Validators.required ,Validators.maxLength(2)],
+      lastName :['',Validators.required],
+      email: ['',Validators.required,Validators.email],
+      password:['',Validators.required],
+      confirmPassword :['',Validators.required],
+      acceptance : [false,Validators.requiredTrue]
+    })
+  }
+  onSubmit(){
+    this.submitted =true;
+    // validators working or not
+if( this.registerForm.invalid ){
+  return;
+}
+console.table(this.registerForm.value)
+console.table(this.registerForm)
+
+alert("Success sign up\n" + JSON.stringify(this.registerForm.value))
+
+  }
+  onReset(){
+    this.submitted =false;
+    this.registerForm.reset()
+  }
 }
